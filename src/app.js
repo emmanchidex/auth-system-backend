f018df42d60e67df8a6306a8af2d5578493a5873
emma <emmanuelfamily6@gmail.com>
2026-08-29 15:59:41 -0700

    Verify database environment variables

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 require("dotenv").config();
 
+console.log({
+  DB_USER: !!process.env.DB_USER,
+  DB_HOST: !!process.env.DB_HOST,
+  DB_NAME: !!process.env.DB_NAME,
+  DB_PASSWORD: !!process.env.DB_PASSWORD,
+  DB_PORT: !!process.env.DB_PORT,
+});
+
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
